2847d86bbeb3bb3c8f6dbae990f9071b476abba3
Tomasz Siekierda <sierdzio@gmail.com>
2015-03-01 20:11:52 +0100

    Small printing style fixes. Rename testDht() to testDhts() to better convey it's functionality.

--- climateStationPrototype/BoardTester.py
+++ climateStationPrototype/BoardTester.py
@@ -1,13 +1,13 @@
 try:
     import RPi.GPIO as GPIO
 except RuntimeError:
-    print("Error could not import RPi.GPIO. You need to run as root, or "
-        "install the module")
+    print "Error could not import RPi.GPIO. You need to run as root, or " \
+    "install the module"
 
 try:
     import Adafruit_DHT as DHT
 except RuntimeError:
-    print("Error: could not import Adafruit_DHT module. Please install it")
+    print "Error: could not import Adafruit_DHT module. Please install it"
 
 import time
 
@@ -17,8 +17,10 @@ class BoardTester:
 
     def __init__(self):
         """Sets the board mode and default pins"""
-        print("Preparing the BoardTester")
+        print "Preparing the BoardTester"
         GPIO.setmode(GPIO.BCM)
+        print "  RaspberryPI version: ", GPIO.RPI_INFO['P1_REVISION']
+        print "  GPIO version: ", GPIO.VERSION
         self.ledPins = [21, 26]
         self.dhtPins = [19, 20]
 
@@ -26,18 +28,18 @@ class BoardTester:
         """Performs all the tests available in this class"""
         self.testLeds()
         time.sleep(0.5)
-        self.testDht()
+        self.testDhts()
 
     def testLeds(self):
         """ This will test LEDs on default ports. Those ports are 21 and 26."""
-        print("Testing LEDs")
+        print "Testing LEDs"
 
         for pin in self.ledPins:
             self.testLed(pin)
 
     def testLed(self, pin):
         """Tests a single LED, mounted to a given pin"""
-        print"Testing LED on pin: ", pin, ". Will blink 10 times"
+        print "  Testing LED on pin: {}. Will blink 10 times".format(pin)
         GPIO.setup(pin, GPIO.OUT)
         count = 10
         while count > 0:
@@ -48,14 +50,15 @@ class BoardTester:
             count = count - 1
         GPIO.cleanup(pin)
 
-    def testDht(self):
+    def testDhts(self):
         """Tests the DHT sensors"""
+        print "Testing DHTs"
         for pin in self.dhtPins:
             humidity, temperature = DHT.read_retry(DHT.AM2302, pin)
             if humidity is not None and temperature is not None:
-                print "Testing DHT on pin: {}".format(pin), \
-                ", temperature: {0:0.1f}, humidity: {0:0.1f}" \
-                .format(temperature, humidity)
+                print "  Testing DHT on pin: {0}, temperature: {1:0.1f}, " \
+                "humidity: {2:0.1f}" \
+                .format(pin, temperature, humidity)
             else:
-                print "Error: could not read from DHT sensor on pin:", pin, \
+                print "  Error: could not read from DHT sensor on pin:", pin, \
                 "Please retry"
--- climateStationPrototype/Test1.py
+++
@@ -1,7 +0,0 @@
-import RPi.GPIO as GPIO
-
-def prepare():
-    print("Hello world")
-
-def testLeds():
-    print("Testing LEDs")
