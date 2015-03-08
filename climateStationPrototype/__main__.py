6844ff973841658efbb6d05f40a2d1c2e2f0c1d6
Tomasz Siekierda (sierdzio) <sierdzio@gmail.com>
2015-03-08 21:12:57 +0100

    Add flag -t and use it for testing file saving. Fix command line help text

--- climateStationPrototype/__main__.py
+++ climateStationPrototype/__main__.py
@@ -7,15 +7,17 @@ tester = CSBoardTester()
 
 if "-h" in argv or "--help" in argv:
     print("Raspberry Pi Climate Station - prototype\n\n"
-        "By default, it runs a test in GPIO mode - and requires a properly"
-        "connected and set-up Raspberry Pi to work. Please pass '-s' or"
+        "By default, it runs a test in GPIO mode - and requires a properly "
+        "connected and set-up Raspberry Pi to work. Please pass '-s' or "
         "'--simulated' to run the test in simulated mode - using the "
         "simulatedDevices.ini file as device list.\n\n"
         "Project maintains partial Python 3 compatibility - that is, you can "
         "run the simulation using Python 2 or Python 3. If you notce any "
         "problems here, please notify the developers. However, due to use of "
         "external libraries, Python 3 is not supported in real device mode, "
-        "when connected to GPIO. Sorry about that.")
+        "when connected to GPIO. Sorry about that.\n\n"
+        "Use '-t' or '--test-file-saving' to save current device list to out "
+        "directory")
 else:
     if "-s" in argv or "--simulated" in argv:
         tester.loadDevices("simulatedDevices.ini")
@@ -24,8 +26,10 @@ else:
     else:
         tester.loadDevices("realDevices.ini")
     tester.test()
-    outFile = "out/deviceSavingTest.ini"
-    print("Testing INI file saving: current file list will be saved to {}"
-        .format(outFile))
-    config = CSConfig()
-    config.saveDevices(tester.manager.devices, outFile)
+
+    if "-t" in argv or "--test-file-saving" in argv:
+        outFile = "out/deviceSavingTest.ini"
+        print("Testing INI file saving: current file list will be saved to {}"
+            .format(outFile))
+        config = CSConfig()
+        config.saveDevices(tester.manager.devices, outFile)
